ca6e77398ee0ee28d71f2ff66dbf6e003da3da1d
bonde ramata <bonderamata@gmail.com>
2023-08-04 21:56:34 +0400

    projet init

---
+++ index.js
@@ -0,0 +1,104 @@
+
+ const bb = document.querySelector(".bb"); 
+
+    const bt = document.querySelector(".bt"); 
+    const ba = document.querySelector(".ba"); 
+    
+    const ab= document.querySelector(".ab"); 
+
+    const ac= document.querySelector(".ac"); 
+    const ad = document.querySelector(".ad"); 
+    
+    const cc= document.querySelector(".cc"); 
+
+    const dd= document.querySelector(".dd"); 
+    const ee = document.querySelector(".ee"); 
+    
+    
+    
+    
+    const maman= document.querySelector(".maman"); 
+    const ma= document.querySelector(".ma"); 
+    const komo= document.querySelector(".komo"); 
+    const man= document.querySelector(".man"); 
+    
+    
+    
+    //    declaration                   
+const minata = document.querySelector(".minata")                         
+const kevine = document.querySelector(".kevine")
+const dia = document.querySelector(".dia")                      
+// bb.addEventListener("click", () =>{
+//     minata.style.display = "flex";
+//  kevine.style.display = "none";
+//  dia.style.display = "none";
+// })
+bt.addEventListener("click", () =>{
+    kevine.style.display = "flex";
+    dia.style.display = "none";
+    minata.style.display = "none";
+ 
+ 
+})
+ab.addEventListener("click", () =>{
+    dia.style.display = "flex";
+    kevine.style.display = "none";
+   
+    minata.style.display = "none";
+ 
+ 
+})
+// ac.addEventListener("click", () =>{
+//     minata.style.display = "flex";
+//  kevine.style.display = "none";
+//  dia.style.display = "none";
+// })
+ad.addEventListener("click", () =>{
+    kevine.style.display = "none";
+    dia.style.display = "flex";
+    minata.style.display = "none";
+ 
+ 
+})
+
+cc.addEventListener("click", () =>{
+    minata.style.display = "flex";
+ kevine.style.display = "none";
+ dia.style.display = "none";
+})
+
+ma.addEventListener("click", () =>{
+    minata.style.display = "flex";
+ kevine.style.display = "none";
+ dia.style.display = "none";
+})
+
+
+
+
+maman.addEventListener("click", () =>{
+    kevine.style.display = "flex";
+    dia.style.display = "none";
+    minata.style.display = "none";
+ 
+ 
+})
+
+
+komo.addEventListener("click", () =>{
+    kevine.style.display = "none";
+    dia.style.display = "flex";
+    minata.style.display = "none";
+ 
+ 
+})
+man.addEventListener("click", () =>{
+    kevine.style.display = "flex";
+    dia.style.display = "none";
+    minata.style.display = "none";
+ 
+ 
+})
+
+
+
